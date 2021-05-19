9a2736d4fc71e8501d69883b23b1c72dc3ffdd28
Elisey <jiraiya106@gmail.com>
2021-05-19 20:19:13 +0300

    Lesson 2

---
+++ clever/src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Header } from '../header';
+import './app.css';
+
+export const App = () => (
+    <div className='app'>
+        <Header />
+        Компонент App</div>
+)
